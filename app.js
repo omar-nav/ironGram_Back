7fe5b818840f4dc4c0aa1ac342619875ef4e2bbb
omar-nav <omar.navarro@g.austincc.edu>
2018-10-04 16:15:57 -0500

    login con tokens

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 require('dotenv').config();
 
-const bodyParser   = require('body-parser');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
+const express = require('express');
+const favicon = require('serve-favicon');
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
 
 
 mongoose.Promise = Promise;
 mongoose
-  .connect(process.env.DB, {useMongoClient: true})
+  .connect(process.env.DB, { useMongoClient: true })
   .then(() => {
     console.log('Connected to Mongo!')
   }).catch(err => {
@@ -42,11 +42,11 @@ app.use(passport.session())
 // Express View engine setup
 
 app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
+  src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
   sourceMap: true
 }));
-      
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
--- routes/auth.js
+++ routes/auth.js
@@ -2,19 +2,26 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
 const passport = require('passport')
+const { generateToken, verifyToken } = require('../helpers/jwt')
 
-router.post('/login',
- passport.authenticate('local'), 
- (req,res,next)=>{
-    res.status(200).json(req.user)
+router.get('/private', verifyToken, (req, res, next) => {
+    res.send("Esto sololo ven los usuarios logueado" + req.user.username)
+
 })
 
-router.post('/signup', (req,res,next)=>{
-    User.register(req.body, req.body.password)
-    .then(user=>{
-        res.status(201).json(user)
+router.post('/login',
+    passport.authenticate('local'),
+    (req, res, next) => {
+        const token = generateToken(req.user)
+        res.status(200).json({ token, user: req.user })
     })
-    .catch(e=>next(e))
+
+router.post('/signup', (req, res, next) => {
+    User.register(req.body, req.body.password)
+        .then(user => {
+            res.status(201).json(user)
+        })
+        .catch(e => next(e))
 })
 
 
